169fec40b42cc9fc583155e9568dd974fe1152af
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-10-14 19:55:51 +0700

    Update sidebar links and add right sidebar with notifications and user profile

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -20,7 +20,7 @@
                     <h3>Main</h3>
                 </li>
                 <li class="slide">
-                    <a class="side-menu__item" data-bs-toggle="slide" href="index.html"><i
+                    <a class="side-menu__item" data-bs-toggle="slide" href="{{route('home')}}"><i
                             class="side-menu__icon fe fe-home"></i><span class="side-menu__label">Dashboard</span></a>
                 </li>
                 <li class="sub-category">
