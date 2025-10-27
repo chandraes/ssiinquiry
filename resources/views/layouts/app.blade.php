7878e668346dd4b682d414407e35e112b566b5c7
UPT TIK Unsri <ka.tik@unsri.ac.id>
2025-10-27 19:10:17 +0700

    update add meta viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- ... (head Anda tidak berubah) ... --}}
     @vite(['resources/sass/app.scss'])
     @php
